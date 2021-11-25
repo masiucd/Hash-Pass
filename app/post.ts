007a9609eab7c5a409c937f4dba8f9f0bb3e5690
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-11-25 14:13:11 +0100

    with markdown

--- app/post.ts
+++ app/post.ts
@@ -1,12 +1,27 @@
 import path from "path"
 import fs from "fs/promises"
 import parseFrontMatter from "front-matter"
+import invariant from "tiny-invariant"
+import {marked} from "marked"
+
 export interface Post {
   slug: string
   title: string
 }
 
+interface FrontMatter {
+  title: string
+  created: string
+  spoiler: string
+  tags: string[]
+}
+
 const postPath = path.join(__dirname, "..", "posts")
+
+function isValidPostAttributes(attributes: any): attributes is FrontMatter {
+  return attributes?.title
+}
+
 export const getPosts = async (): Promise<Post[]> => {
   const dir = await fs.readdir(postPath)
   console.log("dir", dir)
@@ -14,13 +29,22 @@ export const getPosts = async (): Promise<Post[]> => {
 
   return Promise.all(
     dir.map(async fileName => {
-      let file = await fs.readFile(path.join(postPath, fileName))
-      let {attributes} = parseFrontMatter(file.toString())
-      console.log(attributes)
+      const file = await fs.readFile(path.join(postPath, fileName))
+      const {attributes} = parseFrontMatter(file.toString())
+      invariant(isValidPostAttributes(attributes), `${fileName} has bad meta data!`)
       return {
         slug: fileName.replace(/\.md$/, ""),
         title: attributes.title,
       }
     })
   )
+}
+
+export const getPost = async (slug: string) => {
+  // console.log(path.join())
+  const blogPostPath = path.join(postPath, slug + ".md")
+  const file = await fs.readFile(blogPostPath)
+  const {attributes} = parseFrontMatter(file.toString())
+  invariant(isValidPostAttributes(attributes), `Post ${blogPostPath} is missing attributes`)
+  return {slug, title: attributes.title}
 }
---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,20 @@
+import {useLoaderData} from "remix"
+import type {LoaderFunction} from "remix"
+import {getPost, Post} from "~/post"
+import invariant from "tiny-invariant"
+
+export const loader: LoaderFunction = async ({params}) => {
+  invariant(params.slug, "expected params.slug")
+  return getPost(params.slug)
+}
+
+const PostSlug = () => {
+  const post = useLoaderData<Post>()
+  return (
+    <div>
+      <h1> Post {post.title}</h1>
+    </div>
+  )
+}
+
+export default PostSlug
