b6ef5045ec19e524bb7dccdebe3e26ae2653ebac
masiucd <ciszekmarcell@gmail.com>
2022-07-03 14:46:22 +0200

    Temp converter

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 mod helpers;
 mod math;
 mod models;
-use models::dog;
+mod temp;
+
 fn main() {
-  models::user::print_user();
-  dog::doggy();
-
-  let add_by_10 = curry_add(10);
-  let a = add_by_10(20);
-  let b = add_by_10(500);
-  println!("{}", a);
-  println!("{}", b);
-}
-
-fn curry_add(n: i32) -> impl Fn(i32) -> i32 {
-  move |x| x + n
+  temp::temp_converter::run();
 }
