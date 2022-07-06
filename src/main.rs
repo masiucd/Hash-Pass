72dbcaf77e4bc807d805bb710f588cea7776ee63
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-07-06 19:38:07 +0200

    Stack

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,32 @@ mod math;
 mod problem_solving;
 mod util_fns;
 
+#[derive(Debug)]
+struct Stack {
+    items: Vec<i32>,
+    size: usize,
+}
+
+impl Stack {
+    fn new() -> Stack {
+        Stack {
+            items: Vec::new(),
+            size: 0,
+        }
+    }
+    fn push(&mut self, item: i32) {
+        self.items.push(item);
+        self.size += 1;
+    }
+    fn print(&self) {
+        for item in self.items {
+            println!("{} -> ", item);
+        }
+    }
+}
+
 fn main() {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
     config::simple_config();
     let sum = math::addition::add(10, 20);
@@ -16,10 +41,27 @@ fn main() {
 =======
   let xs = vec![2, 4, 7];
   println!("{}", solution(xs));
+=======
+    // config::simple_config();
+    // let sum = math::addition::add(10, 20);
+    // let sum = math::addition::increase_by_one(sum);
+    // let reversed = problem_solving::reverse::reverse_string("hello".to_string());
+    // let x = problem_solving::dummy::print_name_if_even_age(20, "John");
+    // println!("{:?}", sum);
+    // println!("{:?}", reversed);
+    // println!("{:?}", x);
+
+    let stack = Stack::new();
+    stack.push(19);
+    stack.push(22);
+    stack.push(23);
+    stack.print();
+>>>>>>> Stashed changes
 }
 
 #[test]
 fn test_solution() {
+<<<<<<< Updated upstream
   assert_eq!(solution(vec![2, 4, 7]), 4);
   assert_eq!(solution(vec![1, 1, 3, 4]), 1);
   assert_eq!(
@@ -35,4 +77,11 @@ fn test_solution() {
 fn solution(a: Vec<i32>) -> i32 {
   a[(a.len() - 1) / 2]
 >>>>>>> e3d0ab3b1fdd74e3e5c2c7075758d54501cd700c
+=======
+    assert_eq!(solution(10, 7,), 2);
+    assert_eq!(solution(10, 2,), 7);
+}
+fn solution(n: i32, first_number: i32) -> i32 {
+    (n / 2 + first_number) % n
+>>>>>>> Stashed changes
 }
--- target/debug/deps/app-603c5631666ec57a.d
+++ target/debug/deps/app-603c5631666ec57a.d
@@ -1,6 +1,6 @@
-/Users/marcis/masiu/rust-stuff/target/debug/deps/app-603c5631666ec57a: src/main.rs src/config.rs src/math/mod.rs src/math/addition.rs src/problem_solving/mod.rs src/problem_solving/dummy.rs src/problem_solving/reverse.rs
+/Users/marcis/masiu/rust-stuff/target/debug/deps/app-603c5631666ec57a: src/main.rs src/config.rs src/math/mod.rs src/math/addition.rs src/problem_solving/mod.rs src/problem_solving/dummy.rs src/problem_solving/reverse.rs src/util_fns/mod.rs src/util_fns/helpers.rs
 
-/Users/marcis/masiu/rust-stuff/target/debug/deps/app-603c5631666ec57a.d: src/main.rs src/config.rs src/math/mod.rs src/math/addition.rs src/problem_solving/mod.rs src/problem_solving/dummy.rs src/problem_solving/reverse.rs
+/Users/marcis/masiu/rust-stuff/target/debug/deps/app-603c5631666ec57a.d: src/main.rs src/config.rs src/math/mod.rs src/math/addition.rs src/problem_solving/mod.rs src/problem_solving/dummy.rs src/problem_solving/reverse.rs src/util_fns/mod.rs src/util_fns/helpers.rs
 
 src/main.rs:
 src/config.rs:
@@ -9,3 +9,5 @@ src/math/addition.rs:
 src/problem_solving/mod.rs:
 src/problem_solving/dummy.rs:
 src/problem_solving/reverse.rs:
+src/util_fns/mod.rs:
+src/util_fns/helpers.rs:
