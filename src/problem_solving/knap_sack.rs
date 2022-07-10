5defc0425fe59af53e80cfe8949309c840f1c567
masiucd <ciszekmarcell@gmail.com>
2022-07-10 17:39:15 +0200

    knapsack

---
+++ src/problem_solving/knap_sack.rs
@@ -0,0 +1,26 @@
+pub fn knapsack_light(value_1: i32, weight_1: i32, value_2: i32, weight_2: i32, max_w: i32) -> i32 {
+  if weight_1 + weight_2 <= max_w {
+    return value_1 + value_2;
+  } else if weight_1 > max_w && weight_2 > max_w {
+    return 0;
+  } else if weight_1 <= max_w && weight_2 > max_w {
+    return value_1;
+  } else if weight_2 <= max_w && weight_1 > max_w {
+    return value_2;
+  } else if value_1 >= value_2 {
+    return value_1;
+  } else {
+    return value_2;
+  }
+}
+
+#[cfg(test)]
+mod test {
+
+  #[test]
+  fn test_knapsack_light() {
+    assert_eq!(super::knapsack_light(10, 5, 6, 4, 9), 16);
+    assert_eq!(super::knapsack_light(5, 3, 7, 4, 6), 7);
+    assert_eq!(super::knapsack_light(15, 2, 20, 3, 2), 15);
+  }
+}
--- target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284.d
+++ target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284.d
@@ -1,5 +1,5 @@
-/Users/marcis/masiu/rust-stuff/target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284: /Users/marcis/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs
+/Users/masiuciszek/projects/good-stuff/rust-stuff/target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284: /Users/masiuciszek/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs
 
-/Users/marcis/masiu/rust-stuff/target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284.d: /Users/marcis/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs
+/Users/masiuciszek/projects/good-stuff/rust-stuff/target/debug/build/quote-76a07e2dc811d284/build_script_build-76a07e2dc811d284.d: /Users/masiuciszek/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs
 
-/Users/marcis/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs:
+/Users/masiuciszek/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.20/build.rs:
