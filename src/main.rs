25694c5174133fe1c01478e3a8162217d26a99ef
masiucd <ciszekmarcell@gmail.com>
2022-07-17 20:21:41 +0200

    Iterator

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,6 @@ mod data_structures;
 mod problem_solving;
 // use data_structures::list::{List, Listable};
 
-trait Iterator {
-  type Item;
-  fn next(&mut self) -> Option<Self::Item>;
-}
-
 fn main() {
   let xs = vec![1, 2, 3];
   let next = xs.iter().next();
@@ -18,16 +13,4 @@ fn main() {
   for x in xs {
     println!("{}", x);
   }
-}
-
-#[test]
-fn iterator_demonstration() {
-  let xs = vec![1, 2, 3];
-
-  let mut xs_iter = xs.into_iter();
-
-  assert_eq!(xs_iter.next(), Some(1));
-  assert_eq!(xs_iter.next(), Some(2));
-  assert_eq!(xs_iter.next(), Some(3));
-  assert_eq!(xs_iter.next(), None);
 }
