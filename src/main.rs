ae4aaa23d3856a3192993455ea59da582ce76d2f
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-07-19 19:57:44 +0200

    Beautiful string

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use std::collections::HashMap;
+
 mod data_structures;
 mod problem_solving;
-// use data_structures::list::{List, Listable};
+mod utils;
 
 fn main() {
-    let xs = vec![1, 2, 3];
-    let next = xs.iter().next();
-
-    if let Some(num) = next {
-        println!("{}", num);
+    let mut mapper: HashMap<char, i8> = HashMap::new();
+    for c in "marcell".chars() {
+        let n = mapper.entry(c).or_default();
+        *n = *n + 1;
     }
-
-    for x in xs {
-        println!("{}", x);
-    }
+    // get the key with the max value from the mapper
+    let max_key = mapper.iter().max_by_key(|&(_, v)| v).unwrap();
+    println!("{:?}", max_key);
+    println!("{:?}", mapper);
 }
