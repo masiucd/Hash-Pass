7ba59c1c2ec2d370bf6d973714d3f838d565d544
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-07-23 14:45:31 +0200

    Iterator

--- src/iterators/my_iter.rs
+++ src/iterators/my_iter.rs
@@ -1,9 +1,10 @@
-struct Counter {
+#[derive(Debug)]
+pub struct Counter {
     count: u32,
 }
 
 impl Counter {
-    fn new() -> Counter {
+    pub fn new() -> Counter {
         Counter { count: 0 }
     }
 }
@@ -32,5 +33,15 @@ mod tests {
         assert_eq!(counter.next(), Some(4));
         assert_eq!(counter.next(), Some(5));
         assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn test_counter_from_iterator() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(sum, 18);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod data_structures;
 mod exercism;
 mod helpers;
+mod iterators;
+use iterators::my_iter::Counter;
 
 fn main() {
-    let x = exercism::basics::lasagna_exercism::expected_minutes_in_oven();
-    println!("{}", x);
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("{:?}", sum);
 }
